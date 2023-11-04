bcb5d29217efdbed3353db1035859328b716960e
MinhAn <voongocminhan20072002@gmail.com>
2023-11-04 19:15:41 +0700

    Create FirstScreen

--- src/screen/FirstScreen.js
+++ src/screen/FirstScreen.js
@@ -1,24 +1,38 @@
-import react, { useState } from "react";
-import { StyleSheet, View, Text,FlatList } from "react-native-web";
-import { Image } from "react-native";
-import { useEffect } from "react";
-export default function FistScreen() {
-  const [data,setData] = useState([]);
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, FlatList, Image,Pressable } from "react-native-web";
+
+export default function FirstScreen() {
+  
+  const [data, setData] = useState([]);
+
   useEffect(() => {
-    fetch("https://653f47e79e8bd3be29e02682.mockapi.io/lab07Coffe")
+    fetch("https://653f47e79e8bd3be29e02682.mockapi.io/lab07")
       .then((response) => response.json())
-      .then((json) => setData(json));
+      .then((json) => setData(json.slice(0, 3))); // Chỉ hiển thị 3 phần tử đầu tiên
   }, []);
+  const styles = StyleSheet.create({
+    Headers:{
+      textAlign: 'center',
+      fontWeight: 'bold',
+      fontSize: 25,
+      marginVertical: 50,
+    }
+  });
   return (
-    <View>
-        <FlatList
+    <View style={{width:'100%',backgroundColor:'#DEE1E6'}}>
+      <Text style={styles.Headers}  >Welcome to Cafe World</Text>
+      <FlatList
         data={data}
         renderItem={({ item }) => (
-            <Image source={{uri:item.image}} style={{width:100,height:100}}></Image>
+          <Image source={{ uri: item.image }} style={{ marginVertical:30,  borderRadius: 10,
+            marginHorizontal:100, width: 200, height: 70 }} />
         )}
-        keyExtractor={item => item.id}
-        initialNumToRender = {1}
-        />
-        </View>
+        keyExtractor={(item) => item.id}
+        initialNumToRender={1}
+      />
+      <Pressable style={{backgroundColor: '#00BDD6', marginHorizontal: 50,textAlign:'center',justifyContent:'center', marginVertical: 70,minHeight:50,width:300}}> GET START </Pressable>
+    </View>
+    
   );
+
 }
